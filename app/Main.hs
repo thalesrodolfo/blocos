884b87f2070292b4f2d595e891536bf52c45aecc
Rodolfo Araújo <rodolfo@pop-os.localdomain>
2020-02-24 22:21:17 -0300

    Correction: Adding some initial tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Example (runApp)
+import           Application (runApp)
 
 main :: IO ()
 main = runApp
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,30 +7,33 @@ import           Test.Hspec.Wai.JSON
 import           Network.HTTP.Types.Header
 import           Data.Aeson (Value(..), object, (.=))
 
-import           Example (app)
+import           Application (runApp')
+import qualified Web.Scotty                 as Scotty
+
+import Data.Pool
+import Database.PostgreSQL.Simple (close, Connection)
+
+import qualified Data.Configurator as C
+import qualified Data.Configurator.Types as C
+
+import Service
 
 main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = with app $ do
-  describe "GET /" $ do
-    it "responds with 200" $ do
-      get "/" `shouldRespondWith` 200
-
-    it "responds with 'hello'" $ do
-      get "/" `shouldRespondWith` "hello"
-
-    it "responds with 200 / 'hello'" $ do
-      get "/" `shouldRespondWith` "hello" {matchStatus = 200}
-
-    it "has 'Content-Type: text/plain; charset=utf-8'" $ do
-      get "/" `shouldRespondWith` 200 {matchHeaders = ["Content-Type" <:> "text/plain; charset=utf-8"]}
-
-  describe "GET /some-json" $ do
-    it "responds with some JSON" $ do
-      get "/some-json" `shouldRespondWith` expectedJsonResponse
+main = do
+  loadedConf <- C.load [C.Required "application.conf"]
+  dbConf <- makeDbConfig loadedConf
+  case dbConf of
+    Nothing -> putStrLn "Erro: Não foi possível obter as informações de conexão com o DB"
+    Just conf -> do
+      pool <- createPool (newConn conf) close 1 40 10
+      hspec $ with (runApp' pool) $ do
+        describe "GET /api/v1/usuarios" $ do
+          it "responds with some JSON" $ do
+           get "/api/v1/usuarios" `shouldRespondWith` expectedJsonResponse
 
 expectedJsonResponse = 
-  let ResponseMatcher status headers body = [json|{foo: 23, bar: 42}|]
-  in ResponseMatcher status [hContentType <:> "application/json; charset=utf-8"] body
+  let
+    ResponseMatcher status headers body =
+      [json|[{"email":"teste","username":"teste","password":"teste","id":1}]|]
+  in
+    ResponseMatcher status [hContentType <:> "application/json; charset=utf-8"] body
